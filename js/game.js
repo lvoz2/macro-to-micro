d05a27aaf81c4a8314241c4cb4a28db17308ea06
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2022-01-24 10:35:49 +1100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,10 +1,23 @@
 window.Game = {};
+Game.saves = {};
 Game.init = function() {
-}
-Game.load = function() {
-}
-Game.new = function() {
 }
 $(document).ready(function() {
 	Game.init()
 })
+Game.load = function(slot) {
+	if (slot === 0) {
+		Game.setScreen("load")
+	}
+}
+Game.new = function(slot) {
+	if (slot === 0) {
+		Game.setScreen("new")
+	}
+}
+Game.save = function() {
+	localStorage.setItem('mtm-save', btoa(JSON.stringify(Game.saves)));
+}
+Game.loadSave = function() {
+	Game.saves = JSON.parse(atob(localStorage.getItem('mtm-save')));
+}
